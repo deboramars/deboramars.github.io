bc35f153f9bb46b84e808a457e8a3265f68699b6
deboramars <dbra.marques@gmail.com>
2018-12-22 05:28:58 -0300

    css

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ export default App =>
 <div className = "container" >
   <img src="https://scontent.frec3-1.fna.fbcdn.net/v/t1.0-1/p160x160/46241106_1913509228726563_7065157507555000320_n.jpg?_nc_cat=111&_nc_ht=scontent.frec3-1.fna&oh=4d767caf09d18f380aa8107baa99a979&oe=5C99942D" className = "container__image" alt="Imagem do Perfil" ></img>
   <div className = "container__content">
-    <h1>Débora Mars</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident amet inventore impedit nisi molestiae adipisci, vitae esse repudiandae similique quidem neque a magni! Debitis excepturi sunt hic animi sequi. Quas obcaecati modi maxime eveniet, labore quos atque fuga commodi nisi. Iusto reprehenderit aut incidunt sequi tenetur vitae perspiciatis laudantium cupiditate, vero voluptate magni itaque harum consequatur ullam cumque quos et corporis, voluptatibus odio natus beatae exercitationem, provident deleniti! Eaque, dolorum!</p>
+    <h1 className = "container__content__tittle">Débora Mars</h1>
+    <p className = "container__content__paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident amet inventore impedit nisi molestiae adipisci, vitae esse repudiandae similique quidem neque a magni! Debitis excepturi sunt hic animi sequi. Quas obcaecati modi maxime eveniet, labore quos atque fuga commodi nisi. Iusto reprehenderit aut incidunt sequi tenetur vitae perspiciatis laudantium cupiditate, vero voluptate magni itaque harum consequatur ullam cumque quos et corporis, voluptatibus odio natus beatae exercitationem, provident deleniti! Eaque, dolorum!</p>
     <ul className = "container__content_icons">
       <li className = "container__content_icons__github">
         <a className = "container__content_icons__github__link" href="https://github.com/deboramars/" target="_blank">
