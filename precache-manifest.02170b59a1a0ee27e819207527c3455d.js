7752224e5852602dff383792a08dfa8430f10984
deboramars <dbra.marques@gmail.com>
2018-12-22 06:20:15 -0300

    Updates

---
+++ precache-manifest.02170b59a1a0ee27e819207527c3455d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b4f1bf95c4d0c3b794f71a803a0338d",
+    "url": "/deboramars.github.io/static/media/twitter.9b4f1bf9.svg"
+  },
+  {
+    "revision": "e07fcd67d4936e990dad07b397c5f1ab",
+    "url": "/deboramars.github.io/static/media/linkedin.e07fcd67.svg"
+  },
+  {
+    "revision": "cde0c062f2642e8e4253d0455d93592e",
+    "url": "/deboramars.github.io/static/media/github.cde0c062.svg"
+  },
+  {
+    "revision": "608997e3c259960566c5",
+    "url": "/deboramars.github.io/static/js/runtime~main.608997e3.js"
+  },
+  {
+    "revision": "7fe27c41bd199deb28a2",
+    "url": "/deboramars.github.io/static/js/main.7fe27c41.chunk.js"
+  },
+  {
+    "revision": "a8823585499795a20f9d",
+    "url": "/deboramars.github.io/static/js/1.a8823585.chunk.js"
+  },
+  {
+    "revision": "7fe27c41bd199deb28a2",
+    "url": "/deboramars.github.io/static/css/main.84e3f940.chunk.css"
+  },
+  {
+    "revision": "b8bc44513eca55d35ad00d9ab71ef46e",
+    "url": "/deboramars.github.io/index.html"
+  }
+];
